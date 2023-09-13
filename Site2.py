5bbcad3d8faabe9549f1332a5fefaf3f09722c64
Hakan Gunes <100923570+HakanGnes@users.noreply.github.com>
2023-09-13 22:08:08 +0300

    Update Site2.py

--- Site2.py
+++ Site2.py
@@ -85,7 +85,7 @@ def main():
         ilgilenilebilecek_oyuncular(load_data())
     elif selected_option == "Sales Expectation and Performance Analysis":
         df = pd.read_excel("./Transfer.xlsx")
-        df2 = pd.read_excel("./Segment.xlsx")
+        df2 = pd.read_excel("./no_nans_data.xlsx",sheet_name="Sayfa3")
         df6 = pd.concat([df, df2], axis=1)
         oyuncu_kazanc_beklentisi(df6)
     else:
